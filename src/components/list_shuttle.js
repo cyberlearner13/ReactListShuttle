371222499c4a2c2e77e9733e9c2430e8d721d018
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2018-06-03 13:14:22 +0530

    added event listeners

--- src/components/list_shuttle.js
+++ src/components/list_shuttle.js
@@ -2,6 +2,19 @@ import React , {Component} from 'react';
 import style  from '../../styles/shuttle_styles';
 
 export default class ListShuttle extends Component{
+    add(){
+        alert('add');
+    }
+
+    addAll(){
+        alert('addAll');
+    }
+    remove(){
+        alert('remove');
+    }
+    removeAll(){
+        alert('removeAll');
+    }
     render(){
         return(
             <div className = "row" style={style.app}>
@@ -10,28 +23,32 @@ export default class ListShuttle extends Component{
                     <div className="row">
                         <button 
                         className="waves-effect waves-light btn" 
-                        style={{"width":"150px"}}>
+                        style={{"width":"150px"}}
+                        onClick={this.add.bind(this)}>
                             Add
                         </button>
                     </div>
                     <div className="row">
                         <button 
                         className="waves-effect waves-light btn"
-                        style={{"width":"150px"}}>
+                        style={{"width":"150px"}}
+                        onClick={this.addAll.bind(this)}>
                             Add All
                          </button>
                     </div>
                     <div className="row">
                         <button 
                         className="waves-effect waves-light btn" 
-                        style={{"width":"150px"}}>
+                        style={{"width":"150px"}}
+                        onClick={this.remove.bind(this)}>
                             Remove
                         </button>
                     </div>
                     <div className="row">
                         <button 
                         className="waves-effect waves-light btn" 
-                        style={{"width":"150px"}}>
+                        style={{"width":"150px"}}
+                        onClick={this.removeAll.bind(this)}>
                             RemoveAll
                         </button>
                     </div>
