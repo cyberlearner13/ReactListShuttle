a67212acfbd84757945238e76567bf93eb83c73b
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2018-06-03 14:32:21 +0530

    list displayed and value gathered

--- src/components/list_shuttle.js
+++ src/components/list_shuttle.js
@@ -1,7 +1,19 @@
 import React , {Component} from 'react';
+import ReactDOM from 'react-dom';
 import style  from '../../styles/shuttle_styles';
 
 export default class ListShuttle extends Component{
+   
+    constructor(){
+        super();
+        this.state = {
+            originalList : ["Taiwan", "Saudi Arabia", "Egypt", "Australia", "Dominica", "Kazakhstan", "Peru", "Uganda", "Zimbabwe", "Cambodia", "Honduras", "Japan", "Vietnam", "Western Sahara", "Guinea", "Laos", "Barbados", "Mexico", "New Zealand", "India"],
+            countryToAdd: '',
+            countryToRemove: '',
+            selectedToAdd:false,
+            selectedToRemove:false
+        }
+    }
     add(){
         alert('add');
     }
@@ -15,10 +27,26 @@ export default class ListShuttle extends Component{
     removeAll(){
         alert('removeAll');
     }
+    changeState(value,index){
+        this.setState({
+            selectedToAdd: !this.state.selectedToAdd,
+            countryToAdd: value
+        })
+        let node = ReactDOM.findDOMNode(this.refs[index]);
+        node.classList.add('list');
+    }
     render(){
+        console.log(this.state.countryToAdd);
         return(
             <div className = "row" style={style.app}>
-                <div className="col s3 offset-s1" style={style.listBox} ></div>
+                <div className="col s3 offset-s1" style={style.listBox} >
+                    {
+                            this.state.originalList.map((val,ind)=>{
+                                return <div key={ind} ref={ind} 
+                                    onClick={this.changeState.bind(this,val,ind)}>{val}</div> 
+                            })
+                          }
+                </div>
                 <div className="col s2 offset-s1">
                     <div className="row">
                         <button 
--- styles/shuttle_styles.js
+++ styles/shuttle_styles.js
@@ -1,7 +1,11 @@
 const listBox = {
     border:'1px solid #cdcdcd',
-    minHeight:'250px'
+    minHeight:'250px',
+    maxHeight:'250px',
+    overflow:'auto',
+    cursor:'pointer'
 }
+
 const app = {
     margin : '10% auto'
 
